43e6d42d8ea33ebfd9a6dac1f9d0428786492afa
Nahum Fabian <nahum_fabian13@hotmail.com>
2015-04-15 14:12:37 -0600

    Initial Commit with master HTML layout already implemented

---
+++ WebTranslator/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebTranslator.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/css").Include(
+                "~/Content/skel.css",
+                "~/Content/style.css",
+                "~/Content/style-desktop.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include(
+                "~/Scripts/jquery.min.js",
+                "~/Scripts/jquery.dropotron.min.js",
+                "~/Scripts/skel.min.js",
+                "~/Scripts/skel-layers.min.js",
+                "~/Scripts/init.js"
+                ));
+
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
---
+++ WebTranslator/Controllers/TranslateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTranslator.Controllers
+{
+    public class TranslateController : Controller
+    {
+        // GET: Translate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
